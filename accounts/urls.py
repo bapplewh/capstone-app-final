f5230b6780fc510169c049d93e45c1ceac04efbd
bapplewh <bapps.5@gmail.com>
2021-07-18 14:18:41 -0600

    created and styled homepage

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('dashboard/', views.dashboard),
     path('packages/', views.packages),
     path('clients/', views.clients),
 ]
