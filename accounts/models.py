2d006eb422e2b473bdb2675e7658a9b9d3e2e245
bapplewh <bapps.5@gmail.com>
2021-07-18 22:07:55 -0600

    created database table structures and added some sample data into Django admin panel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Package(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    hourly_rate = models.FloatField(null=True)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+    )
+
+    client = models.ForeignKey(Client, null=True, on_delete=models.SET_NULL)
+    package = models.ForeignKey(Package, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
