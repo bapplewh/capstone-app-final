969b689f1ca4ffabe7488d5c3cd8326c995a4205
bapplewh <bapps.5@gmail.com>
2021-07-19 23:16:37 -0600

    added CRUD functionality to dashboard pages - update order, edit client, delete client, create order

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name="dashboard"),
     path('packages/', views.packages, name="packages"),
     path('client/<str:pk_test>/', views.client, name="client"),
+
+    path('create_order/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_client/<str:pk>/', views.deleteClient, name="delete_client"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 
 # Create your views here.
 
@@ -39,3 +40,39 @@ def client(request, pk_test):
     context = {'client': client, 'orders': orders}
 
     return render(request,'accounts/client.html', context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+
+    context = {'form': form}
+
+    return render (request, 'accounts/order-form.html', context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+
+    context = {'form': form}
+
+    return render(request, 'accounts/order-form.html', context)
+
+def deleteClient(request, pk):
+    client = Client.objects.get(id=pk)
+    if request.method == "POST":
+        client.delete()
+        return redirect('dashboard')
+
+    context = {'item': client}
+
+    return render(request, 'accounts/delete-client.html', context)
