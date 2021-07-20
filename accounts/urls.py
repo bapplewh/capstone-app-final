2fcad4118f0a0c0f5e9702ea59571b4c3fbf190a
bapplewh <bapps.5@gmail.com>
2021-07-19 21:25:17 -0600

    connected page urls with django so that navbars will connect to pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.about),
-    path('testimonials/', views.testimonials),
-    path('signup/', views.signup),
-    path('dashboard/', views.dashboard),
-    path('packages/', views.packages),
-    path('client/<str:pk_test>/', views.client),
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('testimonials/', views.testimonials, name="testimonials"),
+    path('signup/', views.signup, name="signup"),
+    path('signin/', views.signup, name="signin"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('packages/', views.packages, name="packages"),
+    path('client/<str:pk_test>/', views.client, name="client"),
 ]
