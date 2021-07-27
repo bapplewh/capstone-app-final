55aeb30de083689df832434b12ecc2a6b7accd1a
bapplewh <bapps.5@gmail.com>
2021-07-26 21:55:20 -0600

    added Create New Order button on individual client page

--- capstone/settings.py
+++ capstone/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--$()#(+1ak1+y+c2)xwv3=mhk#pitw5th8%9ecv7tsch@l21ze'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['capstone-app-final-bb.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -134,5 +134,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
-import django_on_heroku
-django_on_heroku.settings(locals())
+# import django_on_heroku
+# django_on_heroku.settings(locals())
